e916c937ab0c24ba9c3ad3d720de00bb38d65e2e
romaric <sodjifritas@gmail.com>
2025-06-25 17:17:51 +0000

    install cors

--- TaskFlow/backend/server.js
+++ TaskFlow/backend/server.js
@@ -5,15 +5,25 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-const {errorHandler} = require('./middleware/errorMiddleware')
-connectDB();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const cors = require("cors");
 
+connectDB();
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://your-frontend-name.onrender.com",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
-app.use(errorHandler)
+app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`server listining on port ${PORT}`.cyan.underline);
 });
